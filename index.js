a09976815183fac7a3cf04082dd0c1936405bba3
Alisher <Allumati@icloud.com>
2020-02-10 17:20:09 -0500

    fixed start game

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function () {
   createGrid()
   renderBot(currentPosition)
   separate()
   start()
-  document.addEventListener('keydown', function(e){
-    if (e.key === "ArrowRight" || e.key === "ArrowLeft" || e.key === "ArrowDown" || e.key === "ArrowUp") {
-    let targetUl = document.getElementById('moves-container')
-    let moveLi = document.createElement('li')
+  document.addEventListener('keydown', function (e) {
+    const moveLi = document.createElement('li')
     if (e.key === "ArrowRight") {
       move('right')
       moveLi.innerText = 'Right'
@@ -16,11 +14,10 @@ document.addEventListener("DOMContentLoaded", function(){
     } else if (e.key === "ArrowUp") {
       move('up')
       moveLi.innerText = 'Up'
-    } else if (e.key === "ArrowLeft"){
+    } else if (e.key === "ArrowLeft") {
       move('left')
       moveLi.innerText = 'Left'
-    } 
-  }
-})
+    }
+  })
 })
 
